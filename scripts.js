a6804aae211b2a630133dde2826679d9edff314a
Luigi <luigi.tanaka12@gmail.com>
2022-04-06 16:12:43 -0300

    escolha correta do numero de cartas

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let numeroDeCartas;
+let começar = false;
+
+numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
+while (começar !== true) {
+    if (numeroDeCartas > 14 || numeroDeCartas < 4) {
+        alert("Por favor, escolha um número entre 4 e 14");
+        numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
+    } else if (numeroDeCartas % 2 !== 0) {
+        alert("O número de cartas deve ser par!");
+        numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
+    } else {
+        começar = true;
+    }
+}
