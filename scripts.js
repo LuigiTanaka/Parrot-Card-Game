8e0011224a7bc0aeae57c892d7c4377601d6a470
Luigi <luigi.tanaka12@gmail.com>
2022-04-09 00:24:52 -0300

    logica do clique na carta

--- scripts.js
+++ scripts.js
@@ -5,73 +5,120 @@ function comparador() {
 
 //certifica que o número de cartas é valido
 let numeroDeCartas;
-let começar = false;
-numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
-while (começar !== true) {
-    if (numeroDeCartas > 14 || numeroDeCartas < 4) {
-        alert("Por favor, escolha um número entre 4 e 14");
-        numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
-    } else if (numeroDeCartas % 2 !== 0) {
-        alert("O número de cartas deve ser par!");
-        numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
-    } else {
-        começar = true;
+function verificaNumeroDeCartas() {
+    let começar = false;
+    numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
+    while (começar !== true) {
+        if (numeroDeCartas > 14 || numeroDeCartas < 4) {
+            alert("Por favor, escolha um número entre 4 e 14");
+            numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
+        } else if (numeroDeCartas % 2 !== 0) {
+            alert("O número de cartas deve ser par!");
+            numeroDeCartas = Number(prompt("Com quantas cartas você deseja jogar?"));
+        } else {
+            começar = true;
+        }
     }
 }
+    
+
 
 //cria a array de cartas de acordo com o numero de cartas selecionado
-let gifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
-let n = numeroDeCartas/2;
-let i = 0;
 let cartas = [];
-while (i < n) {
-    cartas.push(gifs[i]);
-    cartas.push(gifs[i]);
-    i++
+function criaCartas() {
+    let gifs = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
+    let n = numeroDeCartas/2;
+    let i = 0;
+    while (i < n) {
+        cartas.push({conteudo: `
+                <div class="carta" onclick="viraCarta(this)">
+                    <div class="face frente">
+                        <img src="images/front.png" />
+                    </div>
+                    <div class="face verso">
+                        <img src="images/${gifs[i]}" />
+                    </div>
+                </div>`});
+
+        cartas.push({conteudo: `
+                <div class="carta" onclick="viraCarta(this)">
+                    <div class="face frente">
+                        <img src="images/front.png" />
+                    </div>
+                    <div class="face verso">
+                        <img src="images/${gifs[i]}" />
+                    </div>
+                </div>`});
+        i++
+    }
 }
 
 //embaralha as cartas
 cartas.sort(comparador);
 
 //adiciona as cartas viradas
-let j = 0;
-let container = document.querySelector(".container-cartas");
-while (j < numeroDeCartas) {
-    container.innerHTML += `
-        <div class="carta" onclick="viraCarta(this)">
-            <div class="face frente">
-                <img src="images/front.png" />
-            </div>
-            <div class="face verso">
-                <img src="images/${cartas[j]}" />
-            </div>
-        </div>`
-    j++;
+let container;
+function adicionaCartas() {
+    let j = 0;
+    container = document.querySelector(".container-cartas");
+    while (j < numeroDeCartas) {
+        container.innerHTML += cartas[j].conteudo;
+        j++;
+    }
 }
 
 //estabelece a largura adequada do container de acordo com o numero de cartas
-if (numeroDeCartas === 4) {
-    container.classList.add("largura1");
-} else if (numeroDeCartas === 6) {
-    container.classList.add("largura2");
-} else if (numeroDeCartas === 8) {
-    container.classList.add("largura3");
-} else if (numeroDeCartas === 10) {
-    container.classList.add("largura4");
-} else if (numeroDeCartas === 12) {
-    container.classList.add("largura5");
-} else if (numeroDeCartas === 14) {
-    container.classList.add("largura6");
+function defineLargura () {
+    if (numeroDeCartas === 4) {
+        container.classList.add("largura1");
+    } else if (numeroDeCartas === 6) {
+        container.classList.add("largura2");
+    } else if (numeroDeCartas === 8) {
+        container.classList.add("largura3");
+    } else if (numeroDeCartas === 10) {
+        container.classList.add("largura4");
+    } else if (numeroDeCartas === 12) {
+        container.classList.add("largura5");
+    } else if (numeroDeCartas === 14) {
+        container.classList.add("largura6");
+    }
 }
 
 
-//vira a carta
+//vira carta
+let cartasSelecionadasNaRodada = [];
 function viraCarta(cartaSelecionada) {
     let frente = cartaSelecionada.querySelector(".frente");
     let verso = cartaSelecionada.querySelector(".verso");
-
     verso.classList.add("vira-frente");
     frente.classList.add("vira-verso");
+
+    cartasSelecionadasNaRodada.push(cartaSelecionada);
+    if (cartasSelecionadasNaRodada.length === 2) {
+        if (cartasSelecionadasNaRodada[0].innerHTML !== cartasSelecionadasNaRodada[1].innerHTML) {
+            setTimeout(desviraCartas, 1000);
+        } else {
+            cartasSelecionadasNaRodada = [];
+        }
+    }
+}
+
+//desvira cartas se diferentes
+function desviraCartas() {
+    let frente1 = cartasSelecionadasNaRodada[0].querySelector(".frente");
+    let verso1 = cartasSelecionadasNaRodada[0].querySelector(".verso");
+    verso1.classList.remove("vira-frente");
+    frente1.classList.remove("vira-verso");
+
+    let frente2 = cartasSelecionadasNaRodada[1].querySelector(".frente");
+    let verso2 = cartasSelecionadasNaRodada[1].querySelector(".verso");
+    verso2.classList.remove("vira-frente");
+    frente2.classList.remove("vira-verso");
+    cartasSelecionadasNaRodada = [];
 }
 
 
+verificaNumeroDeCartas();
+criaCartas();
+adicionaCartas();
+defineLargura();
